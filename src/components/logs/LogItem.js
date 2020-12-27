dcfe0afed58e2ded14aec4d2d4d78331af6c60b6
parthpatel133 <prpatel133@gmail.com>
2020-12-27 21:50:12 +0530

    logItem component

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const LogItem = ({log}) => {
+  return (
+    <li className='collection-item'>
+      <div>
+        <a href=''>{log.message}</a>
+      </div>
+    </li>
+  );
+};
+
+LogItem.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+export default LogItem;
